decf21c67fd1e38102b41947ac964e2b569b0114
Nicolas Brosan <nicolasbrosan@hotmail.com>
2023-04-12 00:07:06 -0300

    Add project files.

---
+++ Generic/Program.cs
@@ -0,0 +1,12 @@
+﻿using Generic;
+
+var sum = new SumAll();
+
+Console.Write("Ingrese un número: ");
+var test1 = Console.ReadLine();
+var example1 = Convert.ToInt32(test1);
+Console.Write("Ingrese el segundo número: ");
+var test2 = Console.ReadLine();
+var example2 = Convert.ToInt32(test2);
+
+sum.Sum(example1, example2);
---
+++ Generic/SumAll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class SumAll
+    {
+        public T Sum<T>(T obj1, T obj2)
+        {
+            dynamic num1 = obj1;
+            dynamic num2 = obj2;
+
+            T result = num1 + num2;
+
+            Console.WriteLine($"La suma de {num1} y {num2} es igual a: {result}");
+
+            return result;
+        }
+    }
+}
