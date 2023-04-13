c986c65df2ba64a851fe6a389391789407ebba37
Nicolas Brosan <nicolasbrosan@hotmail.com>
2023-04-13 19:25:54 -0300

    Se crea una clase generica

---
+++ Generic/Generic.cs
@@ -0,0 +1,13 @@
+﻿namespace Generic
+{
+    public class Generic<T>
+    {
+        public T Value { get; set; }
+        public T Value2 { get; set; }
+        public void GenericSum(T obj1, T obj2)
+        {
+            Value = obj1;
+            Value2 = obj2;
+        }
+    }
+}
--- Generic/Program.cs
+++ Generic/Program.cs
@@ -10,3 +10,16 @@ var test2 = Console.ReadLine();
 var example2 = Convert.ToInt32(test2);
 
 sum.Sum(example1, example2);
+
+Console.WriteLine("-----------------------------------------------------------------");
+
+var sum2 = new Generic<int>();
+var example3 = sum2.Value = 10;
+var example4 = sum2.Value = 20;
+
+var result2 = example3 + example4;
+Console.WriteLine($"La suma entre el número {example3} y {example4} es igual a {result2}");
+
+
+
+
